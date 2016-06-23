be259b5117b7abbb9a55f740019e21ee81295d01
jpr71 <joshuapr1@gmail.com>
2016-06-23 17:19:31 -0500

    Fixed bug by settin python env greater than 2.7.3.

--- link_checker.py
+++ link_checker.py
@@ -1,14 +1,15 @@
 import subprocess
 import smtplib
+import sys
 from email.mime.text import MIMEText as text
 
-websites = ["https://www.elegansvariation.org", "http://andersenlab.org"]
+websites = ["https://elegansvariation.org", "http://andersenlab.org"]
 
 for website in websites:
-  website = website.strip("'")
   cmd = ['linkchecker', website]
-  output = subprocess.Popen( cmd, stdout=subprocess.PIPE ).communicate()[0].split('\n\n')
-  print "output: ", output
+  process = subprocess.Popen( cmd, stdout=subprocess.PIPE )
+  output = process.communicate()[0].split('\n\n')
+  
   urls = [x for x in output if x[0:3] == "URL"]
   broken_links = set()
 
@@ -19,22 +20,22 @@ for website in websites:
       if local_url not in broken_links:
         broken_links.add(local_url)
 
-    server = smtplib.SMTP('smtp.gmail.com', 587)
+  server = smtplib.SMTP('smtp.gmail.com', 587)
 
-    server.ehlo()
-    server.starttls()
+  server.ehlo()
+  server.starttls()
 
-    server.ehlo()
-    server.login("andersenlablinkchecker@gmail.com", "cegwas123")
+  server.ehlo()
+  server.login("andersenlablinkchecker@gmail.com", "cegwas123")
 
-    # Send the message via our own SMTP server, but don't include the
-    # envelope header.
-    m = "Hey,\n" + "This is your daily link checker and these links are not working on "+ website.strip("https://") +":\n\t" + '\n\t'.join(broken_links)
-    recipients = ["danielecook@gmail.com", "joshuapr1@gmail.com"]
-    message = text(m)
-    message['From'] = "andersenlablinkchecker@gmail.com"
-    message['To'] = ", ".join(recipients)
-    message['Subject'] = "Broken links in " + website
+  # Send the message via our own SMTP server, but don't include the
+  # envelope header.
+  m = "Hey,\n" + "This is your daily link checker and these links are not working on "+ website.strip("https://") +":\n\t" + '\n\t'.join(broken_links) 
+  recipients = ["danielecook@gmail.com", "joshuapr1@gmail.com"]
+  message = text(m)
+  message['From'] = "andersenlablinkchecker@gmail.com"
+  message['To'] = ", ".join(recipients)
+  message['Subject'] = "Broken links in " + website
 
-    server.sendmail("andersenlablinkchecker@gmail.com", recipients, message.as_string())
-    server.quit()
+  server.sendmail("andersenlablinkchecker@gmail.com", recipients, message.as_string())
+  server.quit()
